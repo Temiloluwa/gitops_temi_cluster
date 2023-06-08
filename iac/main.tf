f3c44aa1c6f5dec4407bbd560bfee10894880450
Temmie <temilolu74@yahoo.co.uk>
2023-06-08 22:07:31 +0200

    added sample tf file

---
+++ iac/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-234233949"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
