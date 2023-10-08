d864d5518a7ccd46255278ab163ea4539a85cf6f
temiloluwa <temireply@gmail.com>
2023-10-08 21:03:18 +0200

    added ingress helm repo

--- cluster/namespaces/nginx-ingress-ctrl/setup-nginx-ingress-controller.sh
+++
@@ -1,7 +0,0 @@
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-
-helm install ingress-nginx ingress-nginx/ingress-nginx \
-    --version 4.8.1 \
-    --namespace nginx-ingress-ctrl \
-    --create-namespace 
