a9e11e7c706344609f1eccaa3427ad26ef4a61e8
Temiloluwa <temilolu74@gmail.com>
2024-05-02 21:30:42 +0200

    added airflow and nginx proxy manager

--- iac/cluster-workers/main.tf
+++ iac/cluster-workers/main.tf
@@ -16,37 +16,37 @@ module "backend-node" {
 }
 
 
-# module "airflow-node" {
-#   source                        = "../modules/compute"
-#   name                          = "airflow-node"
-#   instance_type                 = var.airflow_instance_type
-#   volume_size                   = var.airflow_volume_size
-#   volume_type                   = "gp3"
-#   vpc_id                        = data.aws_vpc.default.id
-#   subnet_id                     = local.subnet_id
-#   security_group_id             = data.aws_security_group.hyc-cluster-sg-tf.id
-#   configuration_script_path     = "scripts/configure_airflow.tpl"
-#   manager_swarm_id              = var.manager_swarm_id
-#   manager_ip                    = var.manager_ip
-#   use_aws_ami                   = false
-#   enable_termination_protection = false
-#   enable_stop_protection        = false
-# }
+module "airflow-node" {
+  source                        = "../modules/compute"
+  name                          = "airflow-node"
+  instance_type                 = var.airflow_instance_type
+  volume_size                   = var.airflow_volume_size
+  volume_type                   = "gp3"
+  vpc_id                        = data.aws_vpc.default.id
+  subnet_id                     = local.subnet_id
+  security_group_id             = data.aws_security_group.hyc-cluster-sg-tf.id
+  configuration_script_path     = "scripts/configure_airflow.tpl"
+  manager_swarm_id              = var.manager_swarm_id
+  manager_ip                    = var.manager_ip
+  use_aws_ami                   = false
+  enable_termination_protection = false
+  enable_stop_protection        = false
+}
 
 
-# module "development-server-node" {
-#   source                        = "../modules/compute"
-#   name                          = "development-server-node"
-#   instance_type                 = var.development_server_instance_type
-#   volume_size                   = var.development_server_volume_size
-#   volume_type                   = "gp3"
-#   vpc_id                        = data.aws_vpc.default.id
-#   subnet_id                     = local.subnet_id
-#   security_group_id             = aws_security_group.hyc-dev-server-sg-tf.id
-#   configuration_script_path     = "scripts/configure_development_server.tpl"
-#   manager_swarm_id              = var.manager_swarm_id
-#   manager_ip                    = var.manager_ip
-#   use_aws_ami                   = false
-#   enable_termination_protection = false
-#   enable_stop_protection        = false
-# }
+module "development-server-node" {
+  source                        = "../modules/compute"
+  name                          = "development-server-node"
+  instance_type                 = var.development_server_instance_type
+  volume_size                   = var.development_server_volume_size
+  volume_type                   = "gp3"
+  vpc_id                        = data.aws_vpc.default.id
+  subnet_id                     = local.subnet_id
+  security_group_id             = aws_security_group.hyc-dev-server-sg-tf.id
+  configuration_script_path     = "scripts/configure_development_server.tpl"
+  manager_swarm_id              = var.manager_swarm_id
+  manager_ip                    = var.manager_ip
+  use_aws_ami                   = false
+  enable_termination_protection = false
+  enable_stop_protection        = false
+}
