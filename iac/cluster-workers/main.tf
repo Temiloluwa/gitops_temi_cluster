9cd86e7bb920431bff9c6d9c04067b3624bfc99c
Temiloluwa <temilolu74@gmail.com>
2024-05-13 18:36:52 +0200

    launch airflow node

--- iac/cluster-workers/main.tf
+++ iac/cluster-workers/main.tf
@@ -16,22 +16,22 @@
 # }
 
 
-# module "airflow-node" {
-#   source                        = "../modules/compute"
-#   name                          = "airflow-node"
-#   instance_type                 = var.airflow_instance_type
-#   volume_size                   = var.airflow_volume_size
-#   volume_type                   = "gp3"
-#   vpc_id                        = data.aws_vpc.default.id
-#   subnet_id                     = local.subnet_id
-#   security_group_id             = data.aws_security_group.hyc-cluster-sg-tf.id
-#   configuration_script_path     = "scripts/configure_airflow.tpl"
-#   manager_swarm_id              = var.manager_swarm_id
-#   manager_ip                    = var.manager_ip
-#   use_aws_ami                   = false
-#   enable_termination_protection = true
-#   enable_stop_protection        = false
-# }
+module "airflow-node" {
+  source                        = "../modules/compute"
+  name                          = "airflow-node"
+  instance_type                 = var.airflow_instance_type
+  volume_size                   = var.airflow_volume_size
+  volume_type                   = "gp3"
+  vpc_id                        = data.aws_vpc.default.id
+  subnet_id                     = local.subnet_id
+  security_group_id             = data.aws_security_group.hyc-cluster-sg-tf.id
+  configuration_script_path     = "scripts/configure_airflow.tpl"
+  manager_swarm_id              = var.manager_swarm_id
+  manager_ip                    = var.manager_ip
+  use_aws_ami                   = false
+  enable_termination_protection = true
+  enable_stop_protection        = false
+}
 
 
 # module "development-server-node" {
