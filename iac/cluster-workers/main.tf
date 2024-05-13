bb1fe6c36dbdda26d9fb846fc911a18da5fa3d8f
Temiloluwa <temilolu74@gmail.com>
2024-05-13 03:55:07 +0200

    nuke development instance

--- iac/cluster-workers/main.tf
+++ iac/cluster-workers/main.tf
@@ -34,19 +34,19 @@
 # }
 
 
-module "development-server-node" {
-  source                        = "../modules/compute"
-  name                          = "development-server-node"
-  instance_type                 = var.development_server_instance_type
-  volume_size                   = var.development_server_volume_size
-  volume_type                   = "gp2"
-  vpc_id                        = data.aws_vpc.default.id
-  subnet_id                     = local.subnet_id
-  security_group_id             = aws_security_group.hyc-dev-server-sg-tf.id
-  configuration_script_path     = "scripts/configure_development_server.tpl"
-  manager_swarm_id              = var.manager_swarm_id
-  manager_ip                    = var.manager_ip
-  use_aws_ami                   = false
-  enable_termination_protection = true
-  enable_stop_protection        = false
-}
+# module "development-server-node" {
+#   source                        = "../modules/compute"
+#   name                          = "development-server-node"
+#   instance_type                 = var.development_server_instance_type
+#   volume_size                   = var.development_server_volume_size
+#   volume_type                   = "gp2"
+#   vpc_id                        = data.aws_vpc.default.id
+#   subnet_id                     = local.subnet_id
+#   security_group_id             = aws_security_group.hyc-dev-server-sg-tf.id
+#   configuration_script_path     = "scripts/configure_development_server.tpl"
+#   manager_swarm_id              = var.manager_swarm_id
+#   manager_ip                    = var.manager_ip
+#   use_aws_ami                   = false
+#   enable_termination_protection = true
+#   enable_stop_protection        = false
+# }
